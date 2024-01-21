badca8096314d27266c77fad1a49f8ce980106e1
fabmich <fabo9@stud.uniza.sk>
2024-01-21 12:23:11 +0100

    FE - ulohaListRender

--- ui/src/webpages/uloha/ulohaList/ulohaListRender.js
+++ ui/src/webpages/uloha/ulohaList/ulohaListRender.js
@@ -1,14 +1,18 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate } from 'react-router-dom';
+import React, {useEffect, useState} from "react";
+import {useNavigate} from 'react-router-dom';
 import {useKeycloak} from "@react-keycloak/web";
 import UlohaServices from "../../../services/ulohaServices";
-
+import "./ulohaListStylesheet.css"
 
 function UlohaListRender() {
     const [listOfUlohy, setListOfUlohy] = useState([]);
     const [loading, setLoading] = useState(true);
     const navigate = useNavigate();
-    const { keycloak, initialized } = useKeycloak();
+    const {keycloak, initialized} = useKeycloak();
+
+    const [findAllRequest, setFindAllRequest] = useState({pageNumber: "0", pageSize: "5"});
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
 
 
     const handleButtonCreateUloha = () => {
@@ -25,8 +29,12 @@ function UlohaListRender() {
         const fetchUlohy = async () => {
             try {
                 const ulohaServices = new UlohaServices();
-                const response = await ulohaServices.findAllUlohy(keycloak.token);
-                setListOfUlohy(response);
+                const response = await ulohaServices.findAllUlohy(null, {
+                    pageNumber: (currentPage - 1).toString(), // Adjust page numbering to start from 0
+                    pageSize: "5", // Adjust this as needed
+                });
+                setListOfUlohy(response.content);
+                setTotalPages(response.totalPages);
             } catch (error) {
                 console.error("Error fetching Zamestnanci:", error);
             } finally {
@@ -36,42 +44,155 @@ function UlohaListRender() {
         fetchUlohy();
     }, []);
 
+
+    const handlePageChange = async (pageNumber) => {
+        try {
+            const ulohaServices = new UlohaServices();
+
+
+            const response = await ulohaServices.findAllUlohy(keycloak.token, {
+                pageNumber: (pageNumber - 1).toString(),
+                pageSize: findAllRequest.pageSize
+            });
+            setListOfUlohy(response.content);
+            setCurrentPage(pageNumber);
+        } catch (error) {
+            console.error("Error fetching Ulohy:", error);
+        }
+    };
+
+    const handleFirstPage = () => {
+        handlePageChange(1);
+    };
+
+    const handlePreviousPage = () => {
+        const newPage = Math.max(1, currentPage - 1);
+        handlePageChange(newPage);
+    };
+
+    const handleNextPage = () => {
+        const newPage = Math.min(totalPages, currentPage + 1);
+        handlePageChange(newPage);
+    };
+
+    const handleLastPage = () => {
+        handlePageChange(totalPages);
+    };
+
+
+    const handlePageSizeChange = async (pageSize) => {
+        const updatedFindAllRequest = {...findAllRequest, pageSize: pageSize.toString()};
+        setFindAllRequest(updatedFindAllRequest);
+
+        if (pageSize.toString() === "Všetky") {
+            updatedFindAllRequest.pageSize = 1000
+        }
+
+
+        try {
+            const ulohaServices = new UlohaServices();
+            const response = await ulohaServices.findAllUlohy(keycloak.token, {
+                pageNumber: 0,
+                pageSize: updatedFindAllRequest.pageSize, // Use the updated pageSize here
+            });
+            setListOfUlohy(response.content);
+            setCurrentPage(1);
+        } catch (error) {
+            console.error("Error fetching Ulohy:", error);
+        }
+    };
+
+    const renderPageSizeDropdown = () => {
+        const pageSizes = ["5", "10", "20", "Všetky"];
+
+        return (
+            <div className="page-size-dropdown">
+                <select
+                    value={findAllRequest.pageSize}
+                    onChange={(e) => handlePageSizeChange(e.target.value)}
+                >
+                    {pageSizes.map((size) => (
+                        <option key={size} value={size}>{size}</option>
+                    ))}
+                </select>
+            </div>
+        );
+    };
+
+
     return (
-        <div>
+        <div className="uloha-list-content">
             <div className="add-button-container">
+                <h2>Zoznam úloh</h2>
+                {renderPageSizeDropdown()}
                 <button className="add-button" onClick={handleButtonCreateUloha}>+</button>
             </div>
 
-            <h2>Uloha List</h2>
-            {loading ? (
-                <p>Loading...</p>
-            ) : (
-                <ul className="ulohy-list">
-                    {listOfUlohy.map((uloha) => (
-                        <li key={uloha.id} className="uloha-item" onClick={() => handleClickUlohaDetail(uloha.id)}>
-                            <div className="row">
-                                <p className="uloha-info">ID: {uloha.id}</p>
-                                <p className="uloha-info">Názov: {uloha.nazov}</p>
-                                <p className="uloha-info">Dátum vytvorenia: {uloha.datumVytvorenia}</p>
-                                <p className="uloha-info">popis: {uloha.popis}</p>
+            <ul className="ulohy-list">
+                <li className="uloha-header-list-item uloha-header">
+                    <div className="uloha-header-info-row">
+                        <div className="uloha-header-column">
+                            <p>ID</p>
+                        </div>
+                        <div className="uloha-header-column">
+                            <p>Nazov</p>
+                        </div>
+                        <div className="uloha-header-column">
+                            <p>Vrstva</p>
+                        </div>
+                        <div className="uloha-header-column">
+                            <p>Deadline</p>
+                        </div>
+                        <div className="uloha-header-column">
+                            <p>Stav Ulohy</p>
+                        </div>
+                    </div>
+                </li>
+                {listOfUlohy.map((uloha) => (
+                    <li key={uloha.id} className="uloha-list-item" onClick={() => handleClickUlohaDetail(uloha.id)}>
+                        <div className="uloha-info-row">
+                            <div className="uloha-info-column">
+                                <p>{uloha.prefix + '-' + uloha.cisloUlohy}</p>
                             </div>
-                            <div className="row">
-                                {/*<p className="uloha-info">Zamestnany od: {uloha.priradenyZamestnanec.meno + " " + uloha.priradenyZamestnanec.priezvisko}</p>*/}
-                                <p className="uloha-info">Deadline: {uloha.deadline}</p>
-                                <p className="uloha-info">Vrstva: {uloha.vrstva}</p>
-                                <p className="uloha-info">Fix Version: {uloha.fixVersion}</p>
-                                {/*<p className="uloha-info">Pozicia: {uloha.zadavatel.meno + " " + uloha.zadavatel.priezvisko}</p>*/}
-                                <p className="uloha-info">Stav úlohy: {uloha.stavUlohy}</p>
-                                <p className="uloha-info">Číslo úlohy: {uloha.cisloUlohy}</p>
+                            <div className="uloha-info-column">
+                                <p>{uloha.nazov}</p>
                             </div>
-                        </li>
-                    ))}
-                </ul>
-            )}
+                            <div className="uloha-info-column">
+                                <p>{uloha.vrstva}</p>
+                            </div>
+                            <div className="uloha-info-column">
+                                <p>{uloha.deadline}</p>
+                            </div>
+                            <div className="uloha-info-column">
+                                <p>{uloha.stavUlohy}</p>
+                            </div>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+
+            <div className="pagination-container">
+                <button className="pagination-button" onClick={handleFirstPage}>
+                    &lt;&lt;
+                </button>
+                <button className="pagination-button" onClick={handlePreviousPage}>
+                    &lt;
+                </button>
+                {/* You can render the page numbers dynamically */}
+                {Array.from({length: totalPages}, (_, index) => (
+                    <button key={index} className="pagination-button" onClick={() => handlePageChange(index + 1)}>
+                        {index + 1}
+                    </button>
+                ))}
+                <button className="pagination-button" onClick={handleNextPage}>
+                    &gt;
+                </button>
+                <button className="pagination-button" onClick={handleLastPage}>
+                    &gt;&gt;
+                </button>
+            </div>
         </div>
     );
-
-
-
 }
+
 export default UlohaListRender;
